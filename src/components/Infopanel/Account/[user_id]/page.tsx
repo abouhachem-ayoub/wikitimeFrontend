1231d405468fd7f2e331e0a1b6499686bb50cf89
Nohaila <abouhachemayoub@gmail.com>
2025-06-04 17:08:07 +0100

    Reset password logic added

--- src/components/Infopanel/Account/[user_id]/page.tsx
+++ src/components/Infopanel/Account/[user_id]/page.tsx
@@ -237,7 +237,7 @@ catch(error){
                         className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="Pseudo"
                         required
-                        value={formData?.pseudo}
+                        value={user.pseudo || formData?.pseudo}
                         onChange={handleInputChange}
                     />
                      {pseudoExists?
