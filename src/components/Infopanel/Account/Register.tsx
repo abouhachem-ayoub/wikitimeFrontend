453e4a843a0055d6eefb410864b6c497c8e7efce
Nohaila <abouhachemayoub@gmail.com>
2025-06-04 17:02:37 +0100

    Reset password logic added

--- src/components/Infopanel/Account/Register.tsx
+++ src/components/Infopanel/Account/Register.tsx
@@ -265,7 +265,7 @@ const registerwithsocials = async(formData:any)=>{
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify({...formData,emailVerified:true})
     });
     let  data = await response.json();
     if (!response.ok) {
