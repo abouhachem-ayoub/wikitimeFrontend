03775512ee9703e02b2f3c6af55aa263d66ea2e4
Nohaila <abouhachemayoub@gmail.com>
2025-06-09 18:10:07 +0100

    go directly to account on reseet password or verify email

--- src/components/Infopanel/Account/Login.tsx
+++ src/components/Infopanel/Account/Login.tsx
@@ -8,7 +8,15 @@ import GoogleLogo from '../../../assets/social-login/google-logo.png'
 import GithubLogo from '../../../assets/social-login/github-logo.png'
 import {getAuth, signInWithPopup, GoogleAuthProvider,FacebookAuthProvider,GithubAuthProvider} from "firebase/auth";
 import { initializeApp } from 'firebase/app';
-const firebaseApp = initializeApp();
+const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+});
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
